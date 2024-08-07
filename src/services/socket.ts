23d28b9c6d9bbc338fba836031eb41c78c242833
NEMYSESx <shakirraza4227@gmail.com>
2024-08-07 22:15:08 +0530

    multiple user location feature added

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,6 +2,8 @@ import { Server, Socket } from "socket.io";
 
 class SocketService {
   private _io: Server;
+  private locations: { [key: string]: { lat: string; long: string } } = {};
+  private orientations: { [key: string]: { alpha: string } } = {};
 
   constructor() {
     console.log("Initializing socket...");
@@ -28,17 +30,23 @@ class SocketService {
         "sendLocation",
         ({ lat, long }: { lat: string; long: string }) => {
           console.log("Getting the location from frontend", socket.id);
-          io.emit("getLocation", [{ lat, long }]);
+          this.locations[socket.id] = { lat, long }; //only updating the data of the specific user
+          io.emit("getLocation", Object.values(this.locations)); //emiting the updated data of specific user to all the users
           console.log("sending the location to the frontend of all users");
         }
       );
 
       socket.on("sendOrientation", (alpha: string) => {
-        io.emit("getOrientation", [{ alpha }]);
+        this.orientations[socket.id] = { alpha };
+        io.emit("getOrientation", Object.values(this.orientations));
       });
       console.log("hii");
       socket.on("disconnect", () => {
         console.log("Socket disconnected", socket.id);
+        delete this.locations[socket.id];
+        delete this.orientations[socket.id];
+        io.emit("getLocation", Object.values(this.locations)); //sending the data so that remaing clients can get it after disconnected
+        io.emit("getOrientation", Object.values(this.orientations));
       });
     });
   }
